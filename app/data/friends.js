54f30b20f31ecaa6d7dcbc01df7e4b8954b74e83
ianbunn <ianrichardbunn@gmail.com>
2019-01-15 11:09:51 -0600

    initial file structure with html semi-completed

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friends = [
+    {
+        "name":"Ian",
+        "photo":"https://avatars2.githubusercontent.com/u/24309176?s=400&u=8000fae7e70ba53315139707bcea2fa148931cb7&v=4",
+        "scores":[
+            5,
+            5,
+            3,
+            3,
+            3,
+            5,
+            4,
+            4,
+            5,
+            1
+        ]
+    }
+]
